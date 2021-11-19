6093f4efdea07ce8da747ef0f213b9905b291758
Limeng <san.limeng@qq.com>
2021-11-19 15:02:19 +0800

    牌型生成器

--- core/main.py
+++ core/main.py
@@ -11,20 +11,10 @@ Create Date: 2021/11/18
 
 from core.poker import *
 from core.utils import *
+from tests.cards import cards_generate
 
 
 # 运行程序
 def run(debug=False):
     if debug:
         pass
-    while True:
-        ls1 = []
-        ls2 = []
-        a = input('请输入牌值：').split(',')
-        b = input('请输入牌值：').split(',')
-        for i in a:
-            ls1.append(Card(i, random.choice(PokerCard.suits)))
-        for i in b:
-            ls2.append(Card(i, random.choice(PokerCard.suits)))
-        print(ls1, '\n', ls2)
-        print(card_compare(ls1, ls2))
---
+++ tests/cards.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+Project Name: Poker-414
+File Name: cards.py
+Author: Tridagger
+Email: san.limeng@qq.com
+Create Date: 2021/11/19
+-------------------------------------------------
+"""
+import random
+
+from core import poker
+
+
+def cards_generate(types=0):
+    cards = list(poker.PokerCard())
+    card_list = []
+    card_dict = {
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': 'J',
+        '12': 'Q',
+        '13': 'K',
+        '14': 'A',
+    }
+    match types:
+        case 0:  # 随机牌型
+            return cards_generate(random.randint(1, 7))
+
+        case 1:  # 生成单牌
+            card_list.append(random.choice(cards))
+            return card_list
+
+        case 2:  # 生成对牌
+            card_list.append(random.choice(cards))
+            card1 = card_list[0]
+            if card1[0] == 'red' or card1[0] == 'black':
+                card_list = [poker.Card('red', 'Joker'), poker.Card('black', 'Joker')]
+            else:
+                suits = list(poker.PokerCard.suits)
+                suits.pop(suits.index(card1[1]))
+                card_list.append(poker.Card(card1[0], random.choice(suits)))
+            return card_list
+
+        case 3:  # 生成顺子
+            start = random.randint(3, 12)
+            number = random.randint(3, 15-start)
+            suits = list(poker.PokerCard.suits)
+            for i in range(number):
+                card_list.append(poker.Card(card_dict[str(start+i)], random.choice(suits)))
+            return card_list
+
+        case 4:  # 生成连对
+            start = random.randint(3, 12)
+            number = random.randint(3, 15 - start)
+            for i in range(number):
+                suits = list(poker.PokerCard.suits)
+                card_list.append(poker.Card(card_dict[str(start + i)], random.choice(suits)))
+                suits.pop(suits.index(card_list[i*2][1]))
+                card_list.append(poker.Card(card_dict[str(start + i)], random.choice(suits)))
+            return card_list
+
+        case 5:  # 生成炸弹
+            cards = cards[:-2]
+            card_list.append(random.choice(cards))
+            suits = list(poker.PokerCard.suits)
+            suits.pop(suits.index(card_list[0][1]))
+            card_list.append(poker.Card(card_list[0][0], random.choice(suits)))
+            suits.pop(suits.index(card_list[1][1]))
+            card_list.append(poker.Card(card_list[0][0], random.choice(suits)))
+            return card_list
+        case 6:  # 生成导弹
+            cards = cards[:-2]
+            card_list.append(random.choice(cards))
+            suits = list(poker.PokerCard.suits)
+            suits.pop(suits.index(card_list[0][1]))
+            card_list.append(poker.Card(card_list[0][0], random.choice(suits)))
+            suits.pop(suits.index(card_list[1][1]))
+            card_list.append(poker.Card(card_list[0][0], random.choice(suits)))
+            suits.pop(suits.index(card_list[2][1]))
+            card_list.append(poker.Card(card_list[0][0], random.choice(suits)))
+            return card_list
+
+        case 7:  # 生成火箭
+            suits = list(poker.PokerCard.suits)
+            card_list.append(poker.Card('A', random.choice(suits)))
+            card_list.append(poker.Card('4', random.choice(suits)))
+            suits.pop(suits.index(card_list[1][1]))
+            card_list.append(poker.Card('4', random.choice(suits)))
+            random.shuffle(card_list)
+            return card_list
+
+        case _:
+            pass
